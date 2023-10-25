3dbb726f92261579355e0259e669c1461c349205
JackLawrenceCRISPR <133519528+JackLawrenceCRISPR@users.noreply.github.com>
2023-10-25 13:43:01 +0100

    GuiPad V1.2.2 - now supports gamepads up to 15 buttons

--- GuiPad.py
+++ GuiPad.py
@@ -5,7 +5,7 @@ GamepadMaps = {
     'rjoycon': [[1, 0], [0, 1, 2, 3, 4, 5, False, False, False, 6, False, 8, 7, False, 9, 10]], 
     'switchpro': [[0, 1, 2, 3, 4, 5], [0, 1, 2, 3, 6, 10, 7, 8, 9, 4, 5, False, False, False, False, 11], [0, 1, 2, 3]],  #12,13,14,15
     'xbox_windows': [[0, 1, 2, 3, 4, 5], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [1, 0]], 
-    'xbox': [[0, 1, 4, 2, 3, 5], [0, 1, 2, 3, 4, 5, 6, 7, 9, 10, 8], [1, 0]], 
+    'xbox': [[0, 1, 4, 2, 3, 5], [0, 1, 2, 3, 4, 5, 6, 7, 10, 8, 9], [1, 0]], 
     'ps4': [[0, 1, 2, 3, 4, 5], [0, 1, 2, 3, 6, 7, 10, 8, 9, 4, 5, False, False, False, False, 11], [3, 2, 0, 1]], 
     'ps5': [[0, 1, 4, 2, 3, 5], [0, 1, 2, 3, 4, 5, False, False, 6, 7, 10, 8, 9], [1, 0]], 
     'xbox_pygame1x': [[0, 1, 5, 3, 2], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 0]], 
@@ -123,7 +123,7 @@ def GuiPad(GuiPadiniFile:str = None):
     def pressB9(): #Right Stick button
         pyautogui.hotkey('alt', 'right')
     def pressB10(): #Undefined
-        print("Button pressed!")
+        pyautogui.press('enter')
     def pressB11(): #Undefined
         print("Button pressed!")
     def pressB12(): #Undefined
@@ -135,7 +135,7 @@ def GuiPad(GuiPadiniFile:str = None):
     def pressB15(): #Undefined
         print("Button pressed!")
 
-    ListOfButtonFunctions = [pressB0,pressB1,pressB2,pressB3,pressB4,pressB5,pressB6,pressB7,pressB8,pressB9]
+    ListOfButtonFunctions = [pressB0,pressB1,pressB2,pressB3,pressB4,pressB5,pressB6,pressB7,pressB8,pressB9,pressB10,pressB11,pressB12,pressB13,pressB14,pressB15]
 
     try:
         if RebindButtons != "false" and RebindButtons != "False" and not RebindButtons is None:
